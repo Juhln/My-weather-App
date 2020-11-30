9d17e89fb7b53770237cdfe0ce9cebcba63ad4a8
Juhln <71849831+Juhln@users.noreply.github.com>
2020-11-30 20:37:39 +0100

    Add forecast js

--- src/index.js
+++ src/index.js
@@ -38,17 +38,60 @@ function showTemperature(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
 
-  document.querySelector("#icon").setAttribute =
-    ("src",
+    document.querySelector("icon").innerHTML =
+    iconElement.setAttribute
+     ("src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
+
   celsiusTemperature = response.data.main.temp;
+}
+
+function formatHours (timestamp) {
+ let date= new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+ return `${hours} : ${minutes}`;
+}
+
+function displayForecast (response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+let forecast = null;
+
+for (let index =0; index <3; index++) {
+forecast = response.data.list[index];
+forecastElement.innerHTML +=  `
+<div class="col-2">
+  <h5>
+  ${formatHours(forecast.dt * 1000)}
+  </h5>
+  <img
+  src= "http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png";
+  alt=" "
+  />
+<div class="weather-forecast-temperature">
+<strong>${Math.round(forecast.main.temp_max)}° / </strong> 
+${Math.round(forecast.main.temp_min)}° 
+</div>
+</div>
+`; 
+}
 }
 
 function searchCity(city) {
   let apiKey = "b65c396828c9033748dfb67e05e521a8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric `;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -96,14 +139,13 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-let iconElement = document.querySelector("icon");
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
+let iconElement = document.querySelector ("icon");
 let celsiusTemperature = null;
 
 searchCity("Valencia");
